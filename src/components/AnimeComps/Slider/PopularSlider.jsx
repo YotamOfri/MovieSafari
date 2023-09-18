43ff3cf02d9c2b0c61b899f8445e9d5e7346ca85
ProPotam <yotamofri.b@gmail.com>
2023-09-18 21:01:59 +0300

    2 Cards In Slider Anime Home page

--- src/components/AnimeComps/Slider/PopularSlider.jsx
+++ src/components/AnimeComps/Slider/PopularSlider.jsx
@@ -40,8 +40,8 @@ export default function MainSlider({ data }) {
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
+          slidesToShow: 2,
+          slidesToScroll: 2,
         },
       },
     ],
