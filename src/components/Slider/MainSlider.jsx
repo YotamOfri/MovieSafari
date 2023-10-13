cafb8b07ed2b0a3ae948da10adba1e6f1ab24bae
ProPotam <yotamofri.b@gmail.com>
2023-10-13 04:26:04 +0300

    Bug Fixes

--- src/components/Slider/MainSlider.jsx
+++ src/components/Slider/MainSlider.jsx
@@ -2,10 +2,12 @@ import Slider from "react-slick";
 import PropTypes from "prop-types";
 import toPascalCase from "../../utils/toPascalCase";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import SliderCard from "./SliderCard";
+import { Link } from "react-router-dom";
 const CustomPrevArrow = ({ currentSlide, slideCount, ...props }) => (
   <button
     {...props}
@@ -22,9 +24,9 @@ const CustomNextArrow = ({ currentSlide, slideCount, ...props }) => (
     <IoIosArrowForward size={60} />
   </button>
 );
-
 export default function MainSlider({ data, TypeLink }) {
   const [isScrolling, setIsScrolling] = useState(false);
+  const [isHover, setIsHover] = useState(false);
   const settings = {
     dots: false,
     infinite: true,
@@ -70,14 +72,47 @@ export default function MainSlider({ data, TypeLink }) {
   };
 
   return (
-    <div className="pt-10 px-8">
-      <h2 className="pb-2 text-2xl font-bold relative">
+    <div className="sm:pt-10 pt-20 px-8">
+      <Link
+        className="group pb-2 pt-6 text-2xl font-bold relative flex items-center w-fit cursor-pointer gap-2"
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
         {TypeLink === "info" && (
-          <span className="text-white rounded-md z-10">
+          <p className="text-white rounded-md z-10">
             Trending {toPascalCase(data[0].media_type)}s
-          </span>
+          </p>
         )}
-      </h2>
+        <AnimatePresence>
+          {isHover && (
+            <div className="flex relative h-8 text-gray-400/80">
+              <motion.div
+                className="absolute w-fit text-xs top-1/3 left-0 "
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: { duration: 0.3, delay: 0.2 },
+                }}
+                exit={{ opacity: 0 }}
+              >
+                Explore
+              </motion.div>
+              <motion.div
+                className="absolute top-[12.5%]"
+                initial={{ x: "auto", opacity: 0.2 }}
+                animate={{
+                  x: 40,
+                  opacity: 1,
+                  transition: { type: "spring", duration: 0.5 },
+                }}
+                exit={{ x: 0, opacity: 0 }}
+              >
+                <i className="bx bx-chevron-right"></i>
+              </motion.div>
+            </div>
+          )}
+        </AnimatePresence>
+      </Link>
       <Slider {...settings} className="custom-slider">
         {data.map((item) => (
           <div key={item.id} className={`z-10 flex gap-5 px-1 cursor-pointer`}>
@@ -99,11 +134,11 @@ MainSlider.propTypes = {
   TypeLink: PropTypes.string,
 };
 CustomPrevArrow.propTypes = {
-  currentSlide: PropTypes.number.isRequired,
-  slideCount: PropTypes.number.isRequired,
+  currentSlide: PropTypes.any,
+  slideCount: PropTypes.any,
 };
 
 CustomNextArrow.propTypes = {
-  currentSlide: PropTypes.number.isRequired,
-  slideCount: PropTypes.number.isRequired,
+  currentSlide: PropTypes.any,
+  slideCount: PropTypes.any,
 };
